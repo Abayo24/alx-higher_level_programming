90efa0856ea33c635482cd3c621abdd8f90b2e6f
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-08-28 15:59:46 +0300

    task9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list:
-        imax = my_list[0]
-        for i in my_list:
-            if i > imax:
-                imax = i
-        return imax
-    return None
+    if len(my_list) == 0:
+        return ("None")
+    x = my_list[0]
+    for i in my_list:
+        if i > x:
+            x = i
+    return (x)
