d2a9d477a3ea1e9ae7932c3f53a92807e98ae11d
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-08-28 15:06:36 +0300

    task5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     listofchars = list(my_string)
     for char in listofchars:
         if char == 'c' or char == 'C':
-            new_string = listofchars.remove(char)
-    return new_string
+             listofchars.remove(char)
+    return listofchars
