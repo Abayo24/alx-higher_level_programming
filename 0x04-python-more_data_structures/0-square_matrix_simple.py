8016bb468f010bc24718ad7b592d34e21d00c762
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2022-09-01 01:53:58 +0300

    task0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    response = []
-    for row in matrix:
-        sub_matrix = map(lambda num: num**2, row)
-        response.append(list(sub_matrix))
-    return response
+    return [[y ** 2 for y in x] for x in matrix]
     
