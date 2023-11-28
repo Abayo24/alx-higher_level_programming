6c02d3d89018c8dc93783f6b93ad55fe456476fb
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2023-11-29 01:22:55 +0300

    remove

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in str:
-        print("{}".format(str).remove(str[n]))
+    if n >= 0:
+        newstr = str[:n] + str[n + 1:]
+        return (newstr)
+    else:
+        return (str)
