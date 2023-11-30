5df7416de311229d5d5c363b5722c79c3664c43b
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2023-11-30 15:10:56 +0300

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
-
-total_arguments = len(sys.argv) - 1
-arguments = sys.argv
-if total_arguments == 0:
-    print("{} arguments.".format(total_arguments))
-elif total_arguments == 1:
-    print("{} argument:".format(total_arguments))
-    for i, arg in enumerate(arguments[1:], start=1):
-        print(f"{i}: {arg}")
-elif total_arguments > 1:
-    print("{} arguments:".format(total_arguments))
-    for i, arg in enumerate(arguments[1:], start=1):
-        print(f"{i}: {arg}")
+    import sys
+    total_arguments = len(sys.argv) - 1
+    arguments = sys.argv
+    if total_arguments == 0:
+        print("{} arguments.".format(total_arguments))
+    elif total_arguments == 1:
+        print("{} argument:".format(total_arguments))
+        for i, arg in enumerate(arguments[1:], start=1):
+            print(f"{i}: {arg}")
+    elif total_arguments > 1:
+        print("{} arguments:".format(total_arguments))
+        for i, arg in enumerate(arguments[1:], start=1):
+            print(f"{i}: {arg}")
