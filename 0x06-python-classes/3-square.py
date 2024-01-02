7e5df1743ad025a77f013293b17d0534e3646fc9
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-01-02 23:00:07 +0300

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,15 +2,18 @@
 """Size validation"""
 
 
-
 class Square:
     """Representation of a square"""
 
     def __init__(self, size=0):
         """Instantiation with size"""
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    def area(self):
+        """area of square method"""
+        return (self.__size)**2
