0424eedcfb75ec3ee94b6895e85572b311f20fec
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-01-02 22:02:55 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """representation of a square"""
+
+    def __init__(self, size=0):
+        """instantiation with size"""
+        self.__size = size
+
