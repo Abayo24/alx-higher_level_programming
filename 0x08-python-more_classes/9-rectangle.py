1a3e8a96e51facde17b53eb9c1ca765502671f6b
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-01-05 16:33:45 +0300

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -74,7 +74,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        """ returns a new Rectangle instance with width == height == size"""
+        """returns the biggest rectangle based on the area"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         elif not isinstance(rect_2, Rectangle):
@@ -88,4 +88,5 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
+        """returns a new Rectangle instance with width == height == size"""
         return cls(size, size)
