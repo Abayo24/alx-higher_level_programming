1cfd75d89a508be93418cbb92becb39e2e2f6b07
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-01-08 23:15:56 +0300

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -15,6 +15,6 @@ def is_same_class(obj, a_class):
     Returns:
     - True if obj is an instance of a_class, False otherwise.
     """
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     return False
