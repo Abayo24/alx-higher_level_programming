59063dd487b37b436c695f695b424edde57280bb
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-01-08 23:49:24 +0300

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""BaseGeometry with Public instance method"""
+
+
+class BaseGeometry():
+    """BaseGeometry with Public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
