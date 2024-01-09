03567a21d339dba25481164f296d62647562d5e3
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-01-09 18:33:14 +0300

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,5 @@
 
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, mode='r', encoding='utf-8') as a_file:
-        """function that reads a text file (UTF8) and prints it to stdout"""
-        print(a_file.read())
+    with open(filename, encoding='utf-8') as a_file:
+        print(a_file.read(), end="")
