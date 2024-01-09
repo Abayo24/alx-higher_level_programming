d00b265a1709aa2fb989f6e70e81492ab8132857
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-01-09 14:30:15 +0300

    100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    def __eq__(self, value):
+        """overrides eq"""
+        return super().__ne__(value)
+
+    def __ne__(self, value):
+        """overrides ne"""
+        return super().__eq__(value)
+
