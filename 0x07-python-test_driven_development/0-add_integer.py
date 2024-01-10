f65e5bf3f64441d836b3ac5638fb848b6d514848
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-01-10 19:24:59 +0300

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,10 +4,12 @@
 
 def add_integer(a, b=98):
     """function that adds 2 integers."""
-    if isinstance(a, int) and isinstance(b, int):
-        return a + b
-    raise TypeError("a must be an integer or b must be an integer")
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    doctest.testmod(i)
