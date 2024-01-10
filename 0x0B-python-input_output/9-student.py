f90287b45cb36692e33a99feb192d9a7b3738492
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-01-10 16:44:25 +0300

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class Student that defines a student"""
+
+
+class Student:
+    """class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """initializes a new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """retuns dictionary representation of student"""
+        return self.__dict__
