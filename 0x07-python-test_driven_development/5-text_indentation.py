c9612b580189366f2063c6fcf4067a3e1e4bc79d
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-01-10 19:50:57 +0300

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for text_indentatiion() function"""
+
+
+def text_indentation(text):
+    """function for indentation a string"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while len(text) > i:
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            print("{}".format(text[i]))
+            print("")
+            i += 1
+            while text[i] == ' ':
+                i += 1
+        print("{}".format(text[i]), end="")
+        i += 1
