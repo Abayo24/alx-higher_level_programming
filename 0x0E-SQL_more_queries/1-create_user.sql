d7e8d06be3485829b279a0e79e7ee00000c8bd71
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-02-15 14:37:56 +0300

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
 -- creates the MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
-GRANT ALL PRIVILRGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILRGES ON *.* TO 'user_0d_1'@'localhost';
