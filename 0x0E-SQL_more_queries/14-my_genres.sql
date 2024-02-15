92bd9733cee8b393698a088254ca3847b62ee193
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-02-15 16:37:40 +0300

    task14 correction

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
-SELECT tv_genres.name
-FROM tv_show_genres
-JOIN tv_shows
-ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_shows.title = "Dexter"
-ORDER BY tv_genres.name ASC;
+SELECT a.name AS name
+FROM tv_genres AS a
+JOIN tv_show_genres AS b
+ON b.genre_id = a.id
+JOIN tv_shows AS c
+ON b.show_id = c.id
+WHERE c.title = 'Dexter'
+ORDER BY a.name ASC;;
