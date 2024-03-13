5a13106ffb4191fdb857ebe4a89e19f0ba65959e
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-03-13 12:47:40 +0300

    typo

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const cLang = 'C is fun';
 const pLang = 'Python is cool';
-const jLang = 'Javascript is amazing';
+const jLang = 'JavaScript is amazing';
 
 console.log(`${cLang}`);
 console.log(`${pLang}`);
