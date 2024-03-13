958ea8103a723a37a16d19d595a6f04453f53c51
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-03-13 12:41:45 +0300

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const cLang = 'C id fun';
+const pLang = 'Python is cool';
+const jLang = 'Javascript is amaizing';
+
+console.log(`${cLang}`);
+console.log(`${pLang}`);
+console.log(`${jLang}`);
