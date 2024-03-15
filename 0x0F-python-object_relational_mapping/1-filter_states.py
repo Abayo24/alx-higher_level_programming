6eb8e7b52deec7e57a3e94011047cd075508f738
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-03-15 14:06:15 +0300

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,6 +10,7 @@ password = sys.argv[2]
 database = sys.argv[3]
 
 if __name__ == "__main__":
+    """main"""
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=username,
