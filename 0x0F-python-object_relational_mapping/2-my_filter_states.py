445146801026f63ed1d1bef36c2c6d71591e721b
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-03-17 11:18:45 +0300

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
                 WHERE name = %s ORDER BY id\
-                ASC".format(state_name))
+                ASC", (state_name,))
 
     rows = cur.fetchall()
     for row in rows:
