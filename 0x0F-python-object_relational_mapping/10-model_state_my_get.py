60dfaf9a0263e2f1992a8dcfdc26d8deb5057611
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-03-17 22:33:55 +0300

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -24,8 +24,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State.id)\
-                   .filter(State.name\
-                                .collate('utf8mb4_bin') == state_name)\
+                   .filter(State.name.collate('utf8mb4_bin') == state_name)\
                    .first()
 
     if state:
