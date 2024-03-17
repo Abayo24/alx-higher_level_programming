1475bc72f02fc1969fb6c3698a16ef56ee1c2521
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-03-17 11:44:44 +0300

    3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
                 WHERE BINARY name = %s ORDER BY id\
-                ASC",(state_name,))
+                ASC", (state_name,))
 
     rows = cur.fetchall()
     for row in rows:
