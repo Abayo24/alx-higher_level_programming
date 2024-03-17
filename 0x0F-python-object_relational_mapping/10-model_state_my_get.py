61944975f1cf01ff0240cc9dead800eb5168fd54
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-03-17 23:59:46 +0300

    obj-func

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -23,12 +23,12 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State.id)\
+    state_id = session.query(State.id)\
                    .filter(State.name.collate('utf8mb4_bin') == state_name)\
                    .first()
 
-    if state:
-        print(state.id)
+    if state_id:
+        print(state_id.id)
     else:
         print("Not Found")
 
