b6631e85c3177054b8a48bfb0f4b16be617914e6
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-03-17 11:42:42 +0300

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays matching name values"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """main function that displays matching name values"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE BINARY name = %s ORDER BY id\
+                ASC",(state_name,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
